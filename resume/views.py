19e6140e145f869a01e7a1a7c7d7650cb49fc736
lvshaobo <lvshaoboftd@gmail.com>
2017-03-06 21:22:18 +0800

    show myself

---
+++ resume/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Hello, Everyone. Welcome to my resume.')
