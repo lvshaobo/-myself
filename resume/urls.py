adf808ff8a11eb9c2f79dd6ab1f6549a6d4574d0
lvshaobo <lvshaoboftd@gmail.com>
2017-03-14 21:48:11 +0800

    ListView

--- resume/urls.py
+++ resume/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 
-from .views import *
+from . import views
+
+from .models import BasicInformation
 
 app_name = 'resume'
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^award$', award, name='award'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^award$', views.AwardView.as_view(template_name="resume/award.html"), name='award'),
 ]
--- resume/views.py
+++ resume/views.py
@@ -5,9 +5,6 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.views import generic
-
-from django.http import HttpResponse
-
 from .models import BasicInformation
 
 class IndexView(generic.ListView):
@@ -16,11 +13,14 @@ class IndexView(generic.ListView):
     context_object_name = "basic_information_list"
 
 
-
 def index(request):
     return render(request, 'resume/index.html',
         context={'basic_information': BasicInformation.objects.all()}
     )
+
+class AwardView(generic.ListView):
+    model = BasicInformation
+    context_object_name = "basic_information_list"
 
 def award(request):
     return render(request, 'resume/award.html',
